422fdf6e781b63aae2e26d8208d7fed41a98cce9
Rahul Singh <55955478+singhrahul7874@users.noreply.github.com>
2023-05-19 14:31:04 +0530

    Update linkedin_mysql_script.sql

--- Exploratory Data Analysis/linkedin_mysql_script.sql
+++ Exploratory Data Analysis/linkedin_mysql_script.sql
@@ -1,79 +1,71 @@
-use linkedln_project;
+-- Comparison of number of jobs across different cities for different levels
+SELECT location, levels, COUNT(job_id) AS Number_Of_Jobs 
+FROM jobs a 
+JOIN details b ON a.details_id = b.details_id
+GROUP BY 1, 2
+ORDER BY 3 DESC;
 
-/*Comparison of number of jobs across different cities for different level */
+-- Generate insights into number of jobs distribution across various industries
+SELECT Industry, COUNT(job_id) AS Num_Of_Jobs 
+FROM company a 
+JOIN jobs b ON a.company_id = b.company_id
+GROUP BY 1
+ORDER BY Num_Of_Jobs;
 
-select location,levels,count(job_id) as Number_Of_Jobs from jobs a join details b
-on a.details_id=b.details_id
-group by 1,2
-order by 3 desc;
+-- Generate insights into number of openings with respect to the current employee count
+-- Number of openings in companies with less than or equal to 100 employees
+SELECT company_name, Employees_Count, COUNT(*) AS num_of_openings 
+FROM company 
+WHERE Employees_Count <= 100
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 10;
 
+-- Generate insights into number of openings with respect to the current employee count
+-- Number of openings in companies with more than 1000 employees
+SELECT company_name, Employees_Count, COUNT(*) AS num_of_openings 
+FROM company 
+WHERE Employees_Count > 1000
+GROUP BY 1, 2
+ORDER BY 3 DESC 
+LIMIT 10;
 
-/*Generate some insight with respect to number of jobs distribution across various industry. For instance, 
-what is the total number of jobs in Software Industry as compared to Marketing*/
+-- Count the number of jobs across different industries and locations
+SELECT industry, location, COUNT(job_id) AS No_Jobs 
+FROM company a 
+JOIN jobs b ON a.company_id = b.company_id
+GROUP BY 2, 1;
 
+-- Some additional insights
+-- Number of jobs across different levels of position
+SELECT levels, COUNT(*) AS num_of_jobs 
+FROM details
+GROUP BY 1
+ORDER BY 2 DESC;
 
-select Industry,count(job_id) as Num_Of_Jobs from company a join jobs b
-on a.company_id=b.company_id
-group by 1
-order by  Num_Of_Jobs;
+-- Number of jobs across different cities
+SELECT location, COUNT(*) AS num_of_jobs  
+FROM jobs
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
 
-/*Generate insights into number of opening with respect to the current employee count - 
-Number of opening in a company with more than 1000 employees 
-in comparison to number of openings in a company with 100 employees*/
-
-select company_name, Employees_Count, count(*) as num_of_openming from company 
-where Employees_Count <= 100
-group by 1 ,2
-order by 3 desc
-limit 10 ;
-
-select company_name, Employees_Count, count(*) as num_of_openming from company 
-where Employees_Count > 1000
-group by 1 ,2
-order by 3 desc 
-limit 10;
-
-/*Count the number of jobs across different industry across different locations. For instance,
- how many Frontend Engineer jobs are there in Bangalore as compared to Data Analytics jobs
- in Bangalore, or how many Data Analytics jobs are there in Bangalore as compared to number 
- of Data Scientists job in Gurgaon - this needs to 
-be done in SQL but presented in the above created Excel dashboard*/
-
-select industry,location,count(job_id) as No_Jobs from company a join jobs b
-on a.company_id=b.company_id
-group by 2,1;
-
--- some insights of our own
--- number of jobs across diffrent level of position.
-
-select levels, count(*) as num_of_jobs from details
-group by 1
-order by 2 desc;
-
- -- number of jobs across diffrent cities
-select location, count(*) as num_of_jobs  from jobs
-group by 1
-order by 2 desc
-limit 10;
-
--- select companies with diffrent employees count, divide the companies as small,medium and large size on the basis of employees count 
--- and only show the company size and their count
-
-with cte as (
-select  company_name, 
-case when Employees_Count <=200 then "small_size Company" 
-when Employees_Count between 500 and 1000 then "medium_size Company"
-else "Large_size Company" 
-end as Company_size from company a join jobs b 
-on a.company_id = b.company_id
-join details c 
-on b.details_id = c.details_id
-group by 1 
-order by 2 )
-select Company_size,count(*) from cte
-group by 1 
-order by 2 desc ;
-
-
-
-
+-- Select companies with different employees count and categorize them as small, medium, and large size
+-- based on employees count, and show the company size and their count
+WITH cte AS (
+    SELECT company_name, 
+    CASE 
+        WHEN Employees_Count <= 200 THEN "Small Size Company" 
+        WHEN Employees_Count BETWEEN 500 AND 1000 THEN "Medium Size Company"
+        ELSE "Large Size Company" 
+    END AS Company_size 
+    FROM company a 
+    JOIN jobs b ON a.company_id = b.company_id
+    JOIN details c ON b.details_id = c.details_id
+    GROUP BY 1 
+    ORDER BY 2
+)
+SELECT Company_size, COUNT(*)
+FROM cte
+GROUP BY 1 
+ORDER BY 2 DESC;
